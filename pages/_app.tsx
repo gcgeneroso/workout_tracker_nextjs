13d199d84c1de18259d87ad7cbbd2914c6ee3c4f
Gian <gcgeneroso@gmail.com>
2024-04-08 14:16:13 -0400

    Update app homepage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,5 @@
+// npm run dev
+
 import "../css/style.css";
 import "../css/form.css";
 import Head from "next/head";
@@ -8,20 +10,20 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Pet Care App</title>
+        <title>Workout App</title>
       </Head>
 
       <div className="top-bar">
         <div className="nav">
           <Link href="/">Home</Link>
-          <Link href="/new">Add Pet</Link>
+          <Link href="/new">Add Workout</Link>
         </div>
 
-        <img
+        {/* <img
           id="title"
           src="https://upload.wikimedia.org/wikipedia/commons/1/1f/Pet_logo_with_flowers.png"
           alt="pet care logo"
-        ></img>
+        ></img> */}
       </div>
       <div className="wrapper grid">
         <Component {...pageProps} />
