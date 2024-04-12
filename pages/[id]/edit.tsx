da598eccf77e7aefab360c8124dc7c4ddba9abbb
Gian <gcgeneroso@gmail.com>
2024-04-12 18:57:07 -0400

    Fix edit workout endpoint

--- pages/[id]/edit.tsx
+++ pages/[id]/edit.tsx
@@ -7,32 +7,31 @@ const fetcher = (url: string) =>
     .then((res) => res.json())
     .then((json) => json.data);
 
-const EditPet = () => {
+const EditWorkout = () => {
   const router = useRouter();
   const { id } = router.query;
   const {
-    data: pet,
+    data: workout,
     error,
     isLoading,
-  } = useSWR(id ? `/api/pets/${id}` : null, fetcher);
+  } = useSWR(id ? `/api/workouts/${id}` : null, fetcher);
 
   if (error) return <p>Failed to load</p>;
   if (isLoading) return <p>Loading...</p>;
-  if (!pet) return null;
+  if (!workout) return null;
 
-  const petForm = {
-    name: pet.name,
-    owner_name: pet.owner_name,
-    species: pet.species,
-    age: pet.age,
-    poddy_trained: pet.poddy_trained,
-    diet: pet.diet,
-    image_url: pet.image_url,
-    likes: pet.likes,
-    dislikes: pet.dislikes,
+  
+  const fullDate = new Date(workout.workoutDate)
+  console.log(fullDate)
+  const workoutMonth = String(fullDate.getMonth() + 1).padStart(2, '0')
+  const workoutDate = String(fullDate.getUTCDate()).padStart(2, '0')
+  let formatted_date = fullDate.getFullYear() + '-' + workoutMonth + '-' + workoutDate
+  const workoutForm = {
+    workoutDate: formatted_date,
+    weight: workout.weight,
   };
 
-  return <Form formId="edit-pet-form" petForm={petForm} forNewPet={false} />;
+  return <Form formId="edit-workout-form" workoutForm={workoutForm} forNewWorkout={false} />;
 };
 
-export default EditPet;
+export default EditWorkout;
